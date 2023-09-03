8fe734be201138add7a041259e31c5f008807f80
stilram19 <bednaouiomar19@gmail.com>
2023-09-03 21:10:37 +0100

    redis + adminer + static_website

---
+++ srcs/requirements/bonus/redis/tools/script.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sed -i "s/bind 127.0.0.1/#bind 127.0.0.1/g" /etc/redis/redis.conf
+
+redis-server --protected-mode no
--- srcs/requirements/wordpress/tools/wp_script.sh
+++ srcs/requirements/wordpress/tools/wp_script.sh
@@ -17,12 +17,20 @@ wp core install --allow-root \
 	--admin_password=$WP_ADMIN_PASSWORD \
 	--admin_email=bednaouiomar19@gmail.com
 
-wp user create --allow-root $WP_USER aminekhallofi@gmail.com --user_pass=$WP_USER_PASSWORD --role=editor
+wp user create --allow-root $WP_USER aminekhallofi@gmail.com --user_pass=$WP_USER_PASSWORD
+
+# redis configuration
+
+wp config set WP_REDIS_HOST $WP_REDIS_HOST --allow-root
+wp config set WP_REDIS_PORT 6379 --raw --allow-root
+wp config set WP_REDIS_CLIENT phpredis --allow-root
+wp plugin install redis-cache --activate --allow-root
+wp redis enable --allow-root
 
 # running the fast cgi process manager in the foreground
 
 echo "php-fpm starting!"
 
-php-fpm7.4 -F -R
+php-fpm7.4 -F
 
 echo "php-fpm failed!"
